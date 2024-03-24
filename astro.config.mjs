628186d2a69782af48380f56b5d5df71365c2f25
jasongannon <jason.gannon@protonmail.com>
2024-03-24 08:52:04 -0400

    lfg

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import netlify from "@astrojs/netlify";
 import alpinejs from "@astrojs/alpinejs";
-
+import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://statosphere.netlify.app',
-  integrations: [mdx(), sitemap(), alpinejs(), tailwind()],
+  integrations: [icon(), mdx(), sitemap(), alpinejs(), tailwind()],
   output: "server",
   adapter: netlify()
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Albert W. Stevens';
-export const SITE_DESCRIPTION = 'Stratosphere Balloon Flights, 1935-1938';
+export const SITE_TITLE = 'Stratosphere';
+export const SITE_DESCRIPTION = 'Honoring Albert W. Stevens: The Pioneer of Stratosphere Exploration and Aerial Photography';
