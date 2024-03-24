0d956621039a2acd13af92ce73b6d0acc4fe2037
jasongannon <jason.gannon@protonmail.com>
2024-03-23 20:54:00 -0400

    df

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Albert W. Stevens';
+export const SITE_DESCRIPTION = 'Stratosphere Balloon Flights, 1935-1938';
---
+++ src/content/blog/first-post.md
@@ -0,0 +1,8 @@
+---
+title: First post
+description: Lorem ipsum dolor sit amet
+pubDate: Jul 08 2022
+updatedDate: 2024-03-23T09:50:54.951Z
+heroImage: /f11.jpg
+---
+sudderlyerer
