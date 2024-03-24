eb44ebc7ec043ea505e9b106723adeea47a50faf
jasongannon <jason.gannon@protonmail.com>
2024-03-23 23:19:48 -0400

    trying to fix shit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import netlify from "@astrojs/netlify";
+import alpinejs from "@astrojs/alpinejs";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://statosphere.netlify.app',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), alpinejs(), tailwind()],
   output: "server",
   adapter: netlify()
 });
--- src/content/blog/first-post.md
+++ src/content/blog/first-post.md
@@ -2,7 +2,6 @@
 title: First post
 description: Lorem ipsum dolor sit amet
 pubDate: Jul 08 2022
-updatedDate: 2024-03-23T09:50:54.951Z
-heroImage: /f11.jpg
+heroImage: '/images/blog/f11.jpg'
 ---
 sudderlyerer
